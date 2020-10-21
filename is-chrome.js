b93581340416038a43db92d844c29213d1eed321
hchiam <hchiam@users.noreply.github.com>
2020-10-20 21:46:27 -0400

    use updated checker and refactor and clean up code

--- is-chrome.js
+++ is-chrome.js
@@ -1,11 +1,35 @@
 (function () {
-  var isChrome =
-    /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  if (isChrome) return;
-  var useChromeElement = document.getElementById("use-chrome");
-  if (!useChromeElement) {
-    useChromeElement = document.createElement("p");
-    document.body.prepend(useChromeElement);
+  if (isChrome()) return;
+  createOrSetMessageToUseChrome();
+
+  function isChrome() {
+    // based on https://stackoverflow.com/questions/4565112/javascript-how-to-find-out-if-the-user-browser-is-chrome/13348618#13348618
+    var winNav = window.navigator;
+    var isIOSChrome = winNav.userAgent.match("CriOS");
+    if (isIOSChrome) return true;
+
+    var isChromium = window.chrome;
+    var vendorName = winNav.vendor;
+    var isIEEdge = winNav.userAgent.indexOf("Edge") > -1;
+    var isOpera = typeof window.opr !== "undefined";
+    var isGoogleChrome =
+      isChromium !== null &&
+      typeof isChromium !== "undefined" &&
+      vendorName === "Google Inc." &&
+      isIEEdge === false &&
+      isOpera === false;
+
+    if (isGoogleChrome) return true;
+
+    return false;
   }
-  useChromeElement.innerText = "For best results, open this page in Chrome.";
+
+  function createOrSetMessageToUseChrome() {
+    var useChromeElement = document.getElementById("use-chrome");
+    if (!useChromeElement) {
+      useChromeElement = document.createElement("p");
+      document.body.prepend(useChromeElement);
+    }
+    useChromeElement.innerText = "For best results, open this page in Chrome.";
+  }
 })();
